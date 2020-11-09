1c5cc6dcc09a245f0edbc75ff83167f547ba47f8
tedfytw1209 <teddy90731@gmail.com>
2020-11-09 13:26:21 +0800

    Settemp-I1

--- DAI.py
+++ DAI.py
@@ -6,13 +6,13 @@ import datetime
 
 ServerURL = 'http://iot.iottalk.tw:9999'      #with non-secure connection
 #ServerURL = 'https://DomainName' #with SSL connection
-Reg_addr = None #if None, Reg_addr = MAC address
-ODF_list = ['CO2-O','Temperature-O1','Temperature-O6','Humidity-O6']
-IDF_list = ['Settemp-O1'] #Tm output
+Reg_addr = 'OrchidTm_generater' #if None, Reg_addr = MAC address
+ODF_list = ['Temperature-O1','Temperature-O6','Humidity-O6']
+IDF_list = ['Settemp-I1'] #Tm output
 
 DAN.profile['dm_name']='Orchidhouse_control'
 DAN.profile['df_list']=ODF_list+IDF_list
-DAN.profile['d_name']= 'OrchidTm_generater' 
+DAN.profile['d_name']= 'OrchidTm_generater01' 
 
 DAN.device_registration_with_retry(ServerURL, Reg_addr)
 #DAN.deregister()  #if you want to deregister this device, uncomment this line
@@ -47,9 +47,12 @@ Past_date = Now_date
 try:
     f = open(Tm_record_file,'r')
     tmp = float(f.read())
-    if tmp < 35 and tmp > 10:
+    if tmp < 35 and tmp > 10 and tmp:
         Tm_output = tmp
     f.close()
+except Exception as e:
+    print('Warning :')
+    print(e)
 #main
 while True:
     Last_time = time.time()
@@ -57,17 +60,22 @@ while True:
         Now_date = datetime.datetime.today()
         #get data
         temp = DAN.pull('Temperature-O1')
-        Day_data.append(temp)
-        if (Now_date != Past_date):
-            tmp_temp = np.average(np.ndarray(Day_data))
+        if temp:
+            Day_data.append(temp[0])
+            
+        if (Now_date.day != Past_date.day):
+            print(Now_date)
+            print('update Trm temperature')
+            tmp_temp = np.average(np.array(Day_data))
             Tm_output = Tm_addup(tmp_temp)
+            print(Tm_output)
             #DAN.push('Settemp-O1',Tm_output)
             Day_data = []
             #save current data
             f = open(Tm_record_file,'w')
             f.write(str(Tm_output))
             f.close()
-        DAN.push('Settemp-O1',Tm_output)
+        DAN.push('Settemp-I1',Tm_output)
         Past_date = Now_date
     except Exception as e:
         print(e)
